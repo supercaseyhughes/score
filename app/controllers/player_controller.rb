adf70fe381edf6a949423acaae783b571f0634de
Casey Hughes <caseyhughes@Caseys-MacBook-Air.local>
2014-01-15 17:12:23 -0800

    done for the day

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -2,4 +2,30 @@ class PlayerController < ApplicationController
 	def index
 		@players = Player.all
 	end
+
+	def new
+		@player = Player.new
+	end
+	
+	def create
+		@player = Player.new(player_params)
+    	if @player.save
+      		@players = Player.all
+     		render "index"
+    	else
+    		render "new"
+   	 end
+	end
+	
+	def update										
+		player = Player.find(params[:player][:id])
+		#new_score = params[:player][:score] + player.score
+		player.update_attributes(player_params)
+		@players = Player.all
+     		render "index"
+	end
+	private
+	def player_params
+		params.require(:player).permit(:name, :score)
+	end
 end
